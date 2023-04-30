5200d36b784cb818a80c0348a722d24a75396de1
SHjalilo <aaaabdjalil@gmail.com>
2023-04-30 20:34:30 +0100

    add 2 functions

--- file.js
+++ file.js
@@ -19,9 +19,9 @@ let tasks =[
 ];
 
 //document.getElementById("tasks").innerHTML="<h2>Hello world</h2>";
-
+function showData(tasks){
 document.getElementById("tasks").innerHTML="";
-
+let index = 0;
 for(task of tasks){
     let content = `
             <!-- Task -->
@@ -40,7 +40,7 @@ for(task of tasks){
                     
                     <!-- Tasks actions -->
                     <div style="display:flex; justify-content:space-between; align-items:center; width:27%;">
-                        <button  class ="circul" style="background-color: rgb(164,0,0); color:white;">
+                        <button onclick="deleteTask(${index});"  class ="circul" style="background-color: rgb(164,0,0); color:white;">
                             <span class="material-symbols-outlined">delete</span>
                         </button>
                         <button  class ="circul" style="background-color: rgb(0,150,30);  color:white;">
@@ -55,4 +55,53 @@ for(task of tasks){
                 <!--// Task //-->
     `;
     document.getElementById("tasks").innerHTML+=content;
+    index++; 
 }
+}
+// calling funcion showData()
+showData(tasks);
+
+// create items 
+
+let btnAdd = document.getElementById("btn-add");
+
+btnAdd.addEventListener("click",function(){
+    //console.log("ok");
+    let data = prompt("Type here ");
+    //let conf = confirm("Confirm or Cancel");
+    //console.log(data);
+    console.log("data is : ",data);
+    // create date 
+    let now = new Date();
+    // month + 1 => count start from O !!
+    let date = `${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}`;
+    
+    let test ={
+        "title":`${data}`,
+        "date":`${date}`,
+        "isDone": false
+    };
+    tasks.push(test);
+    console.log(tasks);
+    showData(tasks);
+});
+
+// delete items 
+
+function deleteTask(index){
+    //console.log(index);
+    let task = tasks[index];
+    let isConfirmed = confirm("Delete this : "+task.title);
+    
+    if(isConfirmed){
+    // splice (indexToDelete, Number_Of_Items_To_Delete_Start_From_Index)
+    tasks.splice(index,1);
+     showData(tasks);
+     }
+}
+
+
+
+
+
+
